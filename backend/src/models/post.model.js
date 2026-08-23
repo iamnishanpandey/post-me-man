78fced45d2e2d95321d68d6b6c81e479358d7822
iamnishanpandey <nishanp993@gmail.com>
2026-08-23 08:16:56 +0545

    adeed time stamps

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
-  image: String,
-  imageFileId: String,
-  caption: String,
-});
+const postSchema = new mongoose.Schema(
+  {
+    image: String,
+    imageFileId: String,
+    caption: String,
+  },
+  { timestamps: true },
+);
 
 const postModel = mongoose.model("post", postSchema);
 
