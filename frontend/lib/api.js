f8e5f21d6367edf276da03d2ce22daffb55f25d2
iamnishanpandey <nishanp993@gmail.com>
2026-08-23 12:54:13 +0545

    api url

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function fetchPosts() {
   const response = await fetch(`${API_URL}/posts`);
